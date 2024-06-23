8db94a74c9d0ebf095fb8131570eb0b883304a9b
SwarajKalbande <swarajkalbande123@gmail.com>
2024-06-23 20:18:25 +0530

    Component not mounting our already existed page loaded by react routers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
-const apikey= process.env.REACT_APP_API_KEY;
+const apikey = process.env.REACT_APP_API_KEY;
+
+
 
 export default class App extends Component {
+  pageSize = 6
+  country="in"
   
   render() {
     return (
       <div>
-        <Navbar/>
-        <News pageSize = {3} API_KEY={apikey} country="in" category="science"/>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+          <Route exact path="/" element={ <News pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="general" /> } />
+            <Route exact path="/general" element={ <News pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="general" /> } />
+            <Route exact path="/business" element={ <News pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="business" /> } />
+            <Route exact path="/entertainment" element={ <News pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="entertainment" /> } />
+            <Route exact path="/health" element={ <News pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="health" /> } />
+            <Route exact path="/science" element={ <News pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="science" /> } />
+            <Route exact path="/sports" element={ <News pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="sports" /> } />
+            <Route exact path="/technology" element={ <News pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="technology" /> } />
+          </Routes>
+        </BrowserRouter>
       </div>
     )
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 // import PropTypes from 'prop-types'
+import {
+    Link
+  } from "react-router-dom";
 
 export class Navbar extends Component {
     // static propTypes = {
@@ -11,26 +14,22 @@ export class Navbar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">NewsMonkey</a>
+                        <Link className="navbar-brand" to="/">NewsMonkey</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                                 </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="/about">About</a>
-                                </li>
-
-                                <li><a className="nav-link" href="/about">Business      </a></li>    
-                                <li><a className="nav-link" href="/about">Entertainment</a></li>    
-                                <li><a className="nav-link" href="/about">General</a></li>    
-                                <li><a className="nav-link" href="/about">Health</a></li>    
-                                <li><a className="nav-link" href="/about">Science</a></li>    
-                                <li><a className="nav-link" href="/about">Sports</a></li>    
-                                <li><a className="nav-link" href="/about">Technology</a></li>    
+                                <li className="nav-item"><Link className="nav-link active" to="/business">Business</Link></li>
+                                <li className="nav-item"><Link className="nav-link active" to="/entertainment">Entertainment</Link></li>
+                                <li className="nav-item"><Link className="nav-link active" to="/general">General</Link></li>
+                                <li className="nav-item"><Link className="nav-link active" to="/health">Health</Link></li>
+                                <li className="nav-item"><Link className="nav-link active" to="/science">Science</Link></li>
+                                <li className="nav-item"><Link className="nav-link active" to="/sports">Sports</Link></li>
+                                <li className="nav-item"><Link className="nav-link active" to="/technology">Technology</Link></li>
                             </ul>
                         </div>
                     </div>
