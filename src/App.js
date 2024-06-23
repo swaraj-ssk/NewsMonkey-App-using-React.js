81b5c110c47b06579652143e24292acc68a8f333
SwarajKalbande <swarajkalbande123@gmail.com>
2024-06-23 21:02:35 +0530

    Done with adding routes in navbar, now app is worth it

--- src/App.js
+++ src/App.js
@@ -21,14 +21,14 @@ export default class App extends Component {
         <BrowserRouter>
           <Navbar />
           <Routes>
-          <Route exact path="/" element={ <News pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="general" /> } />
-            <Route exact path="/general" element={ <News pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="general" /> } />
-            <Route exact path="/business" element={ <News pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="business" /> } />
-            <Route exact path="/entertainment" element={ <News pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="entertainment" /> } />
-            <Route exact path="/health" element={ <News pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="health" /> } />
-            <Route exact path="/science" element={ <News pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="science" /> } />
-            <Route exact path="/sports" element={ <News pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="sports" /> } />
-            <Route exact path="/technology" element={ <News pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="technology" /> } />
+          <Route exact path="/" element={ <News key="/" pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="general" /> } />
+            <Route exact path="/general" element={ <News key="general" pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="general" /> } />
+            <Route exact path="/business" element={ <News key="business" pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="business" /> } />
+            <Route exact path="/entertainment" element={ <News key="entertainment" pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="entertainment" /> } />
+            <Route exact path="/health" element={ <News key="health" pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="health" /> } />
+            <Route exact path="/science" element={ <News key="science" pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="science" /> } />
+            <Route exact path="/sports" element={ <News key="sports" pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="sports" /> } />
+            <Route exact path="/technology" element={ <News key="technology" pageSize={this.pageSize} API_KEY={apikey} country={this.country} category="technology" /> } />
           </Routes>
         </BrowserRouter>
       </div>
