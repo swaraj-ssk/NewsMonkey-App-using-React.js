5878804ed3d16ff648e4ee2bd7e5e614693ba9aa
SwarajKalbande <swarajkalbande123@gmail.com>
2024-06-24 16:23:00 +0530

    Added infinite scroll bar

--- src/components/News.js
+++ src/components/News.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 
 export class News extends Component {
 
     static defaulProps = {
-        country : 'in',
-        pageSize : 9,
-        category : 'general'
+        country: 'in',
+        pageSize: 9,
+        category: 'general'
     }
 
     static propTypes = {
-        country : PropTypes.string,
-        pageSize : PropTypes.number,
-        category : PropTypes.string
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string
     }
 
     constructor(props) {
@@ -30,9 +31,9 @@ export class News extends Component {
         document.title = `${this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)} - NewsMonkey`
     }
 
-    async updateNews(page){
+    async updateNews(page) {
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.API_KEY}&page=${page}&pageSize=${this.props.pageSize}`;
-        this.setState({loading:true})
+        this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({ article: parsedData.articles, totalResults: parsedData.totalResults, loading: false });
@@ -42,14 +43,22 @@ export class News extends Component {
         this.updateNews(1);
     }
 
-    handlePrevClick = async () => {
-        this.setState({page: this.state.page - 1})
-        this.updateNews(this.state.page+1)
-    }
+    // handlePrevClick = async () => {
+    //     this.setState({ page: this.state.page - 1 })
+    //     this.updateNews(this.state.page - 1)
+    // }
 
-    handleNextClick = async () => {
-        this.setState({page: this.state.page + 1})
-        this.updateNews(this.state.page+1)
+    // handleNextClick = async () => {
+    //     this.setState({ page: this.state.page + 1 })
+    //     this.updateNews(this.state.page + 1)
+    // }
+
+    fetchMoreData = async () => {
+        // this.setState({page : this.state.page+1});
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.API_KEY}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({ page: this.state.page + 1, article: this.state.article.concat(parsedData.articles), totalResults: parsedData.totalResults });
     }
 
     render() {
@@ -57,17 +66,28 @@ export class News extends Component {
             <div className="container my-3">
                 <h1 className='text-center my-3'>NewsMonkey - Top {this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)} Headlines</h1>
                 {this.state.loading && <Spinner/>}
-                <div className='row '>
-                    {!this.state.loading && this.state.article?.map((element) => {
-                        return <div className='col-md-4 d-flex justify-content-center' key={element.url + element.title}>
-                            <NewsItem title={element.title ? element.title : ""} desc={element.description ? element.description : ""} imgUrl={element.urlToImage ? element.urlToImage : "https://media.istockphoto.com/id/187925868/vector/newspaper-cover-page.jpg?s=612x612&w=0&k=20&c=SantJnFi_0dCOD_HUXgRSJxagvgL7Wp_F_e-rFSUV_E="} url={element.url} publishedAt={element.publishedAt} author={element.author} source={element.source.name}/>
+                <InfiniteScroll
+                    dataLength={this.state.article.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.article.length !== this.state.totalResults}
+                    loader={<Spinner />}
+                    // endMessage={
+                    //     <p style={{ textAlign: "center" }}>
+                    //         <b>Yay! You have seen it all</b>
+                    //     </p>
+                    // }
+                >
+                    <div className="container">
+                        <div className='row '>
+                            {!this.state.loading && this.state.article?.map((element) => {
+                                return <div className='col-md-4 d-flex justify-content-center' key={element.url + element.title}>
+                                    <NewsItem title={element.title ? element.title : ""} desc={element.description ? element.description : ""} imgUrl={element.urlToImage ? element.urlToImage : "https://media.istockphoto.com/id/187925868/vector/newspaper-cover-page.jpg?s=612x612&w=0&k=20&c=SantJnFi_0dCOD_HUXgRSJxagvgL7Wp_F_e-rFSUV_E="} url={element.url} publishedAt={element.publishedAt} author={element.author} source={element.source.name} />
+                                </div>
+                            })}
                         </div>
-                    })}
-                </div>
-                <div className="d-flex justify-content-between mt-3">
-                    <button type="button" disabled={this.state.page <= 1} onClick={this.handlePrevClick} className="btn btn-dark">&larr; Previous</button>
-                    <button type="button" disabled={!(Math.ceil(this.state.totalResults / this.props.pageSize) >= (this.state.page+1))} onClick={this.handleNextClick} className="btn btn-dark">Next &rarr;</button>
-                </div>
+                    </div>
+
+                </InfiniteScroll>
             </div>
         )
     }
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -5,7 +5,7 @@ export default class Spinner extends Component {
 
   render() {
     return (
-      <div className='text-center'>
+      <div className='text-center my-2'>
         <img src={loading} alt="loading" />
       </div>
     )
